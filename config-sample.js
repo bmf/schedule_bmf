94435f39562c9ccdf4c96e57bf131a5daf37f3f2
_bmf_ <brandon.flade@gmail.com>
2016-09-03 18:46:17 -0400

    added sample config.js

---
+++ config-sample.js
@@ -0,0 +1,17 @@
+var config = {};
+config.twitter = {};
+config.facebook = {};
+config.meetup = {};
+
+//twitter
+config.twitter.consumer_key = '';
+config.twitter.consumer_secret = '';
+config.twitter.access_token_key = '';
+config.twitter.access_token_secret = '';
+
+//meetup
+config.meetup.apikey = '';
+config.meetup.consumer_key = '';
+config.meetup.consumer_secret = '';
+
+module.exports = config;
